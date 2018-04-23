ae110f40c235e004d08b5d5d29f66bdac46bca47
ensallee <ensallee@gmail.com>
2018-04-23 19:07:07 -0400

    done

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,66 @@
+require 'pry'
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.map do |element|
+    element.sub!(element[2], "$")
+  end
+end
+
+def find_a(array)
+  array.select do |element|
+    element.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject do |sum, n|
+    sum + n
+  end
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index != 1
+      element << "s"
+    else
+      element
+    end
+  end
+end
